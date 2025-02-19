db42da31b6f71ae75cf0278352a17912f6dcf56a
Spencer Albrecht <s.spencer.a@gmail.com>
2025-02-18 20:34:31 -0800

    add twitter conv tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -92,6 +92,15 @@ export default async function RootLayout({
             })(window, document, "clarity", "script", "pqvyzv5zkm");
           `}
         </Script>
+        {/* Twitter conversion tracking base code */}
+        <Script id="twitter-tracking" strategy="afterInteractive">
+          {`
+            !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+            twq('config','oyx5r');
+          `}
+        </Script>
         {/* Google tag (gtag.js) */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-16874049011"
